6d52a085f8d3dac93665368318927a3f41d52cbe
Cameron West <camonater62@gmail.com>
2023-04-28 17:55:54 -0700

    simple class style

--- osu.hpp
+++ osu.hpp
@@ -19,16 +19,32 @@ namespace osu
         return res;
     }
 
-    struct HitObject
+    class HitObject
     {
+    public:
         int time;
         bool hit;
+
+        std::string toString() const
+        {
+            return std::to_string(time) + " " + std::to_string(hit);
+        }
     };
+
+    std::ostream &operator<<(std::ostream &os, const HitObject &obj)
+    {
+        os << obj.toString();
+        return os;
+    }
 
     class Song
     {
     public:
-        Song(std::string osufile);
+        Song::Song(std::string osufile)
+        {
+            processOsuFile(osufile);
+            time = 0.0f;
+        }
 
     private:
         int leadin;
@@ -36,86 +52,85 @@ namespace osu
         std::vector<HitObject> hitObjects;
 
     public:
-        void Update(float fElapsedTime);
-        int TimeMillis();
-        int LeadIn();
-        std::vector<HitObject> &GetHitObjects();
+        void Song::Update(float fElapsedTime)
+        {
+            time += fElapsedTime;
+        }
+
+        int Song::TimeMillis() const
+        {
+            return int(time * 1000.0f);
+        }
+
+        int Song::LeadIn() const
+        {
+            return leadin;
+        }
+
+        const std::vector<HitObject> &
+        Song::GetHitObjects() const
+        {
+            return hitObjects;
+        }
 
     private:
-        void processOsuFile(std::string osufile);
-    };
-
-    Song::Song(std::string osufile)
-    {
-        processOsuFile(osufile);
-        time = 0.0f;
-    }
-
-    void Song::Update(float fElapsedTime)
-    {
-        time += fElapsedTime;
-    }
-
-    int Song::TimeMillis()
-    {
-        return int(time * 1000.0f);
-    }
-
-    int Song::LeadIn()
-    {
-        return leadin;
-    }
-
-    std::vector<HitObject> &
-    Song::GetHitObjects()
-    {
-        return hitObjects;
-    }
-
-    void Song::processOsuFile(std::string osufile)
-    {
-        std::ifstream osu(osufile);
-
-        std::hash<std::string> hasher;
-        srand(hasher(osufile));
-
-        const auto GeneralFunc = [&](std::string line)
+        void Song::processOsuFile(std::string osufile)
         {
-            if (line.rfind("AudioLeadIn") == 0)
-            {
-                leadin = stoi(line.substr(line.find(':') + 1));
-            }
-        };
+            std::ifstream osu(osufile);
 
-        const auto HitObjFunc = [&](std::string line)
-        {
-            std::vector<std::string> args = split(line, ",");
-            int time = stoi(args[2]) + leadin;
-            HitObject obj;
-            obj.time = time;
-            obj.hit = false;
-            hitObjects.push_back(obj);
-        };
+            std::hash<std::string> hasher;
+            srand(hasher(osufile));
 
-        std::string line = "";
-        std::string section = "";
-        while (getline(osu, line))
-        {
-            if (line.length() < 1)
-                continue;
+            const auto GeneralFunc = [&](std::string line)
+            {
+                if (line.rfind("AudioLeadIn") == 0)
+                {
+                    leadin = stoi(line.substr(line.find(':') + 1));
+                }
+            };
 
-            if (line[0] == '[')
+            const auto HitObjFunc = [&](std::string line)
             {
-                section = line.substr(1, line.find_last_of(']') - 1);
-            }
-            else if (section == "General")
+                std::vector<std::string> args = split(line, ",");
+                int time = stoi(args[2]) + leadin;
+                HitObject obj;
+                obj.time = time;
+                obj.hit = false;
+                hitObjects.push_back(obj);
+            };
+
+            std::string line = "";
+            std::string section = "";
+            while (getline(osu, line))
             {
-                GeneralFunc(line);
-            }
-            else if (section == "HitObjects")
-            {
-                HitObjFunc(line);
+                if (line.length() < 1)
+                    continue;
+
+                if (line[0] == '[')
+                {
+                    section = line.substr(1, line.find_last_of(']') - 1);
+                }
+                else if (section == "General")
+                {
+                    GeneralFunc(line);
+                }
+                else if (section == "HitObjects")
+                {
+                    HitObjFunc(line);
+                }
             }
         }
+    };
+
+    std::ostream &operator<<(std::ostream &os, const Song &song)
+    {
+        os << "LeadIn: " << song.LeadIn() << std::endl;
+        os << "Time: " << song.TimeMillis() << std::endl;
+        os << "HitObjects: " << std::endl;
+        for (const auto &obj : song.GetHitObjects())
+        {
+            os << obj << std::endl;
+        }
+        return os;
     }
 }
